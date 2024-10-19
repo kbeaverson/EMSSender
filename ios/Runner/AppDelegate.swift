6775d88ad08c0030d696f34f255499930c0d735b
Kenny <kenny.beaverson@gmail.com>
2024-10-19 13:35:04 -0400

    minor functional updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    // GeneratedPluginRegistrant.register(with: self)
+    // return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return true;
   }
 }
