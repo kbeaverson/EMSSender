0023b7a1ef6626ec6c23d66f4ea00ba426780f1e
Kenny <kenny.beaverson@gmail.com>
2024-10-19 15:32:28 -0400

    Home page foundation

---
+++ lib/emergencyMessage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class EmergencyMessage extends StatelessWidget {
+  const EmergencyMessage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      child: Text("EMERGENCY VEHICLE INBOUND"),
+    );
+  }
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red[300],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Material(
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment.center,
+                height: 15.h,
+                width: 75.w,
+                child: Text("EMS User"),
+              ),
+            ),
+          ),
+          Material(
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment.center,
+                height: 15.h,
+                width: 75.w,
+                child: Text("Public User"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
